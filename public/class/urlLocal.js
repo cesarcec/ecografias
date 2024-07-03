bf445cdd8f063d4695f759edea276032a675a486
francisco <vacafrancisco8@gmail.com>
2024-07-03 07:05:14 -0400

    dominio

--- public/class/urlLocal.js
+++ public/class/urlLocal.js
@@ -1,2 +1,5 @@
-const URL_WEB = 'http://localhost/ecografias/public/';
-const URL_API_REST = 'http://localhost/ecografias/public/api/';
+// const URL_WEB = 'http://localhost/ecografias/public/';
+// const URL_API_REST = 'http://localhost/ecografias/public/api/';
+
+const URL_WEB = 'http://cedisa.bo/';
+const URL_API_REST = 'http://cedisa.bo/api/';
